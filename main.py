05c43eedcae3e9a3ab952937996722dec1073b55
Ivelin Peychev <peychev.vn@gmail.com>
2023-05-04 11:27:49 +0300

    Day 31: add URL pattern for station all data and yearly for station

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ def about(station, date):
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {'station': station, 'date': date, 'temperature': temperature}
 
+@app.route('/api/v1/<station>')
+def all_for_station(station):
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient='records')
+    return result
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly(station, year):
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient='records')
+    return result
 
 # All flask apps runs on port 5000 by default, so if we have multiple apps running on the same time we should specify it
 if __name__ == '__main__':
